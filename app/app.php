d9283eca516c01717dc17fbe484e15e77a59b294
Ben Pritchard <ben.s.pritchard@gmail.com>
2015-10-09 15:32:41 -0700

    Database setup attempt #4.

--- app/app.php
+++ app/app.php
@@ -25,7 +25,7 @@
     $server = $url["host"];
     $username = $url["user"];
     $password = $url["pass"];
-    $db = substr($url["path"], 1);
+    // $db = substr($url["path"], 1);
 
     $DB = new PDO($server, $username, $password);
     // =========================================================================
